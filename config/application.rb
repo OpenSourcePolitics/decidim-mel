890ea237b9df06240e761d8ea7fc602cb4647ae8
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2021-08-24 18:01:35 +0200

    add tests and fix

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module DevelopmentApp
 
     # This needs to be set for correct images URLs in emails
     # DON'T FORGET to ALSO set this in `config/initializers/carrierwave.rb`
-    config.action_mailer.asset_host = "https://participation.lillemetropole.fr"
+    config.action_mailer.asset_host = "https://participation.lillemetropole.fr" unless Rails.env.test?
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,7 +9,7 @@ CarrierWave.configure do |config|
   config.enable_processing = !Rails.env.test?
   # This needs to be set for correct attachment file URLs in emails
   # DON'T FORGET to ALSO set this in `config/application.rb`
-  config.asset_host = "https://participation.lillemetropole.fr"
+  config.asset_host = "https://participation.lillemetropole.fr" unless Rails.env.test?
 end
 
 # Setup CarrierWave to use Amazon S3. Add `gem "fog-aws" to your Gemfile.
