6ddb4cf6ad1cb9ac559bdaef311afe60da0a7e52
moustachu <moustachu@opensourcepolitics.eu>
2021-09-01 10:29:46 +0200

    :robot: rubocop

--- config/initializers/mel_template.rb
+++ config/initializers/mel_template.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Decidim.content_blocks.register(:newsletter_template, :mel_template) do |content_block|
   content_block.cell = "decidim/newsletter_templates/mel_template"
   content_block.settings_form_cell = "decidim/newsletter_templates/mel_template_settings_form"
