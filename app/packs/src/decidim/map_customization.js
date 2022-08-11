46c0242b754ba860a0ab14c5c4a10d9c26eed0c1
paulinebessoles <pauline.bessoles@opensourcepolitics.eu>
2022-08-11 18:05:41 +0200

    Bump 0.25

---
+++ app/packs/src/decidim/map_customization.js
@@ -0,0 +1,31 @@
+((exports) => {
+  const $ = exports.$; // eslint-disable-line
+
+  exports.Decidim = exports.Decidim || {};
+
+  $(() => {
+    let $mapElements = $("[data-decidim-map]");
+
+    $mapElements.on("ready.decidim", (event, map, mapConfig) => {
+
+      fetch("/data/mel.json")
+      .then(response => response.json())
+      .then(geojson =>  {
+        const geojsonLayer = L.geoJSON(geojson, {
+          style: function () {
+            return {
+              color: "#ff1515",
+              weight: 1,
+              fillOpacity: 0.05
+            };
+          }
+        }).addTo(map);
+
+        if (mapConfig.markers.length == 0) {
+          map.fitBounds(geojsonLayer.getBounds());
+        }
+      });
+
+    });
+  });
+})(window);
