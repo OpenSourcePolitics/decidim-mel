38baaa7399a4d1c6fc3bda61d0270ef412c24f3f
Quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2022-09-14 17:34:49 +0200

    Fix session_controller extend for system users

--- lib/extends/controllers/decidim/devise/sessions_controller_extends.rb
+++ lib/extends/controllers/decidim/devise/sessions_controller_extends.rb
@@ -2,9 +2,11 @@
 
 module SessionControllerExtends
   def after_sign_in_path_for(user)
-    if user.present? && user.blocked?
+    return super if user.is_a? Decidim::System::Admin
+
+    if user.present? && user.respond_to?(:blocked?) && user.blocked?
       check_user_block_status(user)
-    elsif first_login_and_not_authorized?(user) && !user.admin? && !pending_redirect?(user) && !skip_authorization_handler?
+    elsif !user.admin? && !pending_redirect?(user) && !skip_authorization_handler? && first_login_and_not_authorized?(user)
       decidim_verifications.first_login_authorizations_path
     else
       super
