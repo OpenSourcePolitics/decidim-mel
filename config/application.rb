2733dc0fb27aa2d3ee5f4362b7ce2a4fed25893d
Armand Fardeau <armandfardeau@users.noreply.github.com>
2023-06-19 18:32:28 +0200

    Refactor migrations tools (#329)
    # Conflicts:
    #	config/application.rb
    #	lib/tasks/k8s.rake
    #	lib/tasks/scaleway.rake

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,7 @@ require "decidim/rails"
 require "action_cable/engine"
 # require "action_mailbox/engine"
 # require "action_text/engine"
+require_relative "../lib/active_storage/downloadable"
 
 # TODO : add missing dep to decidim-initiatives/lib/decidim/initiatives/engine.rb
 # require "wicked_pdf"
@@ -19,7 +20,7 @@ module DevelopmentApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.time_zone = "Europe/Paris"
+    config.time_zone = "Europe/Paris" unless Rails.env.test?
     config.i18n.load_path += Dir[Rails.root.join("config/locales/**/*.yml").to_s]
 
     # This needs to be set for correct images URLs in emails
@@ -34,13 +35,22 @@ module DevelopmentApp
       "X-Content-Type-Options" => "nosniff"
     }
 
-    config.after_initialize do
-      require "extends/controllers/decidim/editor_images_controller_extends"
-    end
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.to_prepare do
+      ActiveStorage::Blob.include ActiveStorage::Downloadable
+    end
+
+    config.after_initialize do
+      require "extends/controllers/decidim/devise/sessions_controller_extends"
+      require "extends/controllers/decidim/editor_images_controller_extends"
+
+      Decidim::GraphiQL::Rails.config.tap do |config|
+        config.initial_query = "{\n  deployment {\n    version\n    branch\n    remote\n    upToDate\n    currentCommit\n    latestCommit\n    locallyModified\n  }\n}".html_safe
+      end
+    end
   end
 end
