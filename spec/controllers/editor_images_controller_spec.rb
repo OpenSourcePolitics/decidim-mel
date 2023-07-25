889b6039ae6cca0198701f6705a4b1da648a54c2
quentinchampenois <quentin.champenois7@gmail.com>
2023-07-25 10:18:40 +0200

    fix: Fix editor images specs

--- spec/controllers/editor_images_controller_spec.rb
+++ spec/controllers/editor_images_controller_spec.rb
@@ -6,7 +6,7 @@ module Decidim
   describe EditorImagesController, type: :controller do
     routes { Decidim::Core::Engine.routes }
 
-    let(:organization) { create(:organization) }
+    let(:organization) { create(:organization, host: "test.host") }
     let(:editor_images_path) { Rails.application.routes.url_helpers.editor_images_url(organization.open_data_file.blob, only_path: true) }
     let(:user) { create(:user, :confirmed, organization: organization) }
     let(:admin) { create(:user, :confirmed, :admin, organization: organization) }
