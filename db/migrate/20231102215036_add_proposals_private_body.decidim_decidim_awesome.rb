f5baf941553765c2af050784336d7aaabe14773b
moustachu <moustachu@opensourcepolitics.eu>
2023-11-03 08:10:35 +0100

    rubocop

--- db/migrate/20231102215036_add_proposals_private_body.decidim_decidim_awesome.rb
+++ db/migrate/20231102215036_add_proposals_private_body.decidim_decidim_awesome.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This migration comes from decidim_decidim_awesome (originally 20230309124413)
 class AddProposalsPrivateBody < ActiveRecord::Migration[5.2]
   def change
