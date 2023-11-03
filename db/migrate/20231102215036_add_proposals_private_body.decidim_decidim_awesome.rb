04b29432900ec6ad8cb8d85f3486759a5266a25e
moustachu <moustachu@opensourcepolitics.eu>
2023-11-03 07:38:03 +0100

    Add missing migration

---
+++ db/migrate/20231102215036_add_proposals_private_body.decidim_decidim_awesome.rb
@@ -0,0 +1,6 @@
+# This migration comes from decidim_decidim_awesome (originally 20230309124413)
+class AddProposalsPrivateBody < ActiveRecord::Migration[5.2]
+  def change
+    add_column :decidim_proposals_proposals, :private_body, :text, null: false, default: "<xml></xml>"
+  end
+end
