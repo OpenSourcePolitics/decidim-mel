9dba016048fc312c44e9d3a8f0cc0d4366ad5c68
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2023-11-03 16:27:14 +0100

    fix: reversibility private body

--- db/migrate/20231102215036_add_proposals_private_body.decidim_decidim_awesome.rb
+++ db/migrate/20231102215036_add_proposals_private_body.decidim_decidim_awesome.rb
@@ -2,7 +2,7 @@
 
 # This migration comes from decidim_decidim_awesome (originally 20230309124413)
 class AddProposalsPrivateBody < ActiveRecord::Migration[5.2]
-  def change
+  def up
     add_column :decidim_proposals_proposals, :private_body, :text, null: false, default: "<xml></xml>"
   end
 end
